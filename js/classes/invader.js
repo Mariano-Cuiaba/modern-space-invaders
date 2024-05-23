b7622da34c1ad4dbf58ed9b5ce744dc7aa201e5c
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-23 14:22:33 -0400

    correção dom invader class

--- js/classes/invader.js
+++ js/classes/invader.js
@@ -1,21 +1,21 @@
 class Invader {
-    constructor({ position }) {
-        this.velocity = {
-            x: 0,
-            y: 0
-        };
+  constructor({ position }) {
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
 
-        const image = new Image();
-        image.src ="./img/invader.png";
-        image.onload = () => {
-            const scale = 1
-            this.image = image;
-            this.width = this.image.width * scale;
-            this.height = this.image.height * scale;
-            this.position = {
-                x: position.x,
-                y: position.y
-            }
-        }
-    }
+    const image = new Image();
+    image.src = "./img/invader.png";
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = this.image.width * scale;
+      this.height = this.image.height * scale;
+      this.position = {
+        x: position.x,
+        y: position.y
+      };
+    };
+  }
 }
