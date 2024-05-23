48bc036fcc93b4df1d460f763e72bf5a33ffa9d5
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-23 14:55:09 -0400

    add update function in InvaderProjectile class

--- js/classes/InvaderProjectile.js
+++ js/classes/InvaderProjectile.js
@@ -10,4 +10,10 @@ class InvaderProjectile {
         caches.fillStyle = "white";
         caches.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
 }
