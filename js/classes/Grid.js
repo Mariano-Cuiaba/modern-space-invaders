af8f9ce91e6ab8d780e1f178c8411e24bea7f41d
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-05-23 13:52:15 -0400

    add update function

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -19,11 +19,22 @@ class Grid {
             for (let j = 0; j < rows; j++) {
                 this.invaders.push(new Invader({
                     postition: {
-                        i: i *30,
+                        i: i * 30,
                         j: j * 30
                     }
                 }));
             }
+        }
+    }
+
+    update(){
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.velocity.y = 0;
+
+        if(this.postition.x + this.width >= CanvasCaptureMediaStreamTrack.width || this.position.x <= 0){
+            this.velocity.x = -this.velocity.x * 1.15;
+            this.velocity.y = 30;
         }
     }
 }
