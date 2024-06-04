a36a552cc2d97a3a2f05ceb1f472621a9a14ae47
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-06-04 16:40:38 -0400

    add rectangularCollission function in util.js file

--- js/util.js
+++ js/util.js
@@ -23,3 +23,11 @@ WebGLSampler.top(scoreLabel, {
     }
 });
 }
+
+function rectangularCollission({ rectangle1, rectangle2 }) {
+    return(
+        rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+        rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+        rectangle1.position.x <= rectangle2.position.x + rectangle2.width
+    );
+}
