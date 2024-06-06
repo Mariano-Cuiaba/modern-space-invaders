1783899797cb7ab6b254f54e8cbbd12ec0d878bf
Mariano-Cuiaba <educlanmariano@gmail.com>
2024-06-06 17:25:32 -0400

    add updates and removes invader projectiles that leave the screen and checks for collisions between projectiles and the player

--- index.js
+++ index.js
@@ -188,4 +188,27 @@ function animate() {
             particle.update();
         }
     });
+
+    invaderProjectiles.forEach((invaderProjectile, index) => {
+        if (
+            invaderProjectile.position.y + invaderProjectile.height >=
+            canvas.height
+        ) {
+            setTimeout(() => {
+                invaderProjectiles.splice(index, 1);
+            }, 0);
+        } else {
+            invaderProjectile.update();
+        }
+
+        if(
+            rectangularCollission({
+                rectangle1: invaderProjectile,
+                rectangle2: player
+            })
+        ){
+            invaderProjectiles.splice(index, 1);
+            endGame();
+        }
+    });
 }
